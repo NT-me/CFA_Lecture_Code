d643ed8d9054158eb0c51ccdb77c0521482ed133
ratchet9364 <axel.bouvier@outlook.fr>
2021-04-26 14:52:02 +0200

    start

--- src/SyntaxicalParser/SynParser.java
+++ src/SyntaxicalParser/SynParser.java
@@ -1,4 +1,16 @@
 package SyntaxicalParser;
 
+import Tools.Node;
+import Tools.Token;
+
+import java.util.ArrayList;
+
 public class SynParser {
+    //ArrayList<Node> token = new ArrayList<Node>();
+    public ArrayList<Node> parsing(ArrayList<Token> token){
+        ArrayList<Node> AST = new ArrayList<Node>();
+
+        return AST;
+    }
 }
+
