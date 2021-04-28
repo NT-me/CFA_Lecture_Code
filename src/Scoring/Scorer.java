92a64aa4315726ee5ff02b3548f7bdc85c1e60b0
Axel <silver-bird@hotmail.fr>
2021-04-28 10:16:01 +0200

    Convention de nommage Axel

--- src/Scoring/Scorer.java
+++ src/Scoring/Scorer.java
@@ -9,8 +9,12 @@ import java.util.Locale;
 
 public class Scorer {
 
+    /**
+     * @param ast of type Node, Ast of the source file that contains the program
+     * @return a list of type ArrayList<String>, all the names of all the variable contained on the program
+     */
     private static ArrayList<String> extractVarDeclarationsInList(Node ast){
-        ArrayList<String> res = new ArrayList<String>();
+        ArrayList<String> res = new ArrayList<>();
 
         if (ast instanceof DeclarationNode){
             DeclarationNode currentNode = (DeclarationNode) ast;
@@ -28,14 +32,34 @@ public class Scorer {
 
     }*/
 
+    /**
+     *
+     * @param ast of type Node
+     * @return result
+     */
     public static ArrayList<String> varNameConventions(Node ast){
-        ArrayList<String> retErrors = new ArrayList<String>();
+        ArrayList<String> retErrors = new ArrayList<>();
 
         ArrayList<String> varDeclList = extractVarDeclarationsInList(ast);
 
         for (String decl: varDeclList){
             if (!decl.equals(decl.toLowerCase())){
                 // Cas où la variable n'est pas en minuscule
+                retErrors.add(decl);
+            }
+        }
+
+        return retErrors;
+    }
+
+    public static ArrayList<String> varConstConventions(Node ast){
+        ArrayList<String> retErrors = new ArrayList<>();
+
+        ArrayList<String> varDeclList = extractVarDeclarationsInList(ast);
+
+        for (String decl: varDeclList){
+            if (!decl.equals(decl.toUpperCase())){
+                // Cas où la variable (supposée un constante) n'est pas en Majuscule
                 retErrors.add(decl);
             }
         }
